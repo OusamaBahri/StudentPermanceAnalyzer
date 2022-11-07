a960996070e8738f8bf1de2211c8e4368bea41c9
OusamaBahri <bahri.oussama@rocketmail.com>
2022-11-07 14:58:12 +0100

    try modal feature

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 import streamlit as st
 import pandas as pd
+import streamlit_modal as modal
+import streamlit.components.v1 as components
 st.set_page_config(page_title="PerformanceAnalyzer", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
 st.markdown("<h1 style='text-align: center; color: grey;'>Welcome to Student Performance Analyzer</h1>", unsafe_allow_html=True)
+cols = st.columns(5)
+with cols[4]:
+    open_modal = st.button("Open")
+    if open_modal:
+        modal.open()
 
+    if modal.is_open():
+        with modal.container():
+            st.write("Text goes here")
 
+            html_string = '''
+            <h1>HTML string in RED</h1>
+
+            <script language="javascript">
+            document.querySelector("h1").style.color = "red";
+            </script>
+            '''
+            components.html(html_string)
+
+            st.write("Some fancy text")
+            value = st.checkbox("Check me")
+            st.write(f"Checkbox checked: {value}")
 
 def main_page():
     st.markdown("# Main page 🎈")
--- pages/page_3.py
+++ pages/page_3.py
@@ -3,18 +3,35 @@ import pandas as pd
 import plotly.express as px
 # exploring categorical data
 # try modal-popup
-st.sidebar.header("Categorical Data")
-st.sidebar.markdown("Select the categorical features you want to explore")
+import streamlit_modal as modal
+
+import streamlit.components.v1 as components
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+@st.experimental_singleton
 def load_data():
     return pd.read_csv('data/StudentsPerformance.csv')
 
 data = load_data()
 categorical_features = data.select_dtypes(include=['object']).columns
-selected_categorical_features = st.sidebar.multiselect("Select", categorical_features)
-if len(selected_categorical_features) > 0:
-    st.markdown("## Categorical Features")
-    st.dataframe(data[selected_categorical_features])
+if len(categorical_features) > 0:
+    #st.markdown("## Categorical Features")
+    #st.dataframe(data[selected_categorical_features])
     st.markdown("## Count Plot")
-    for col in selected_categorical_features:
+    for col in categorical_features:
         fig = px.histogram(data, x=col, title=f"Count of {col}")
         st.plotly_chart(fig)
+
+cols = st.columns(2)
+
+
+open_modal = st.button("Open")
+if open_modal :
+    modal.open()
+
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
